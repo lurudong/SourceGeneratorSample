68af308b22640df2772a5f02bf43be9f168cd0f1
dong <44517714+lurudong@users.noreply.github.com>
2023-12-13 20:51:19 +0800

    1

--- src/SourceGeneratorSample.IncrementalGenerator/GreetingGenerator_UseAttribute.cs
+++ src/SourceGeneratorSample.IncrementalGenerator/GreetingGenerator_UseAttribute.cs
@@ -16,83 +16,71 @@ public sealed class GreetingGenerator_UseAttribute : IIncrementalGenerator
         //{
         //    Debugger.Launch();
         //}
-        context.RegisterSourceOutput(
-                context.SyntaxProvider.ForAttributeWithMetadataName("SourceGeneratorSample.SayHello2Attribute",
-                 static (node, _) => node is MethodDeclarationSyntax
-                 {
-                     Modifiers: var modifiers and not [],
-                     Parent: TypeDeclarationSyntax
-                     {
-                         Modifiers: var typeModifiers and not []
-                     }
-                 }
-                 && modifiers.Any(SyntaxKind.PartialKeyword)
-                 && typeModifiers.Any(SyntaxKind.PartialKeyword),
-                 (gasc, _) =>
-                 {
-
-                     if (gasc is not
-                         {
-                             TargetNode: MethodDeclarationSyntax node,
-                             TargetSymbol: IMethodSymbol
-                             {
-                                 Name: var methodName,
-                                 TypeParameters: [],
-                                 Parameters: [
-                                     {
-                                         Type.SpecialType: SpecialType.System_String,
-                                         Name: var parameterName
-                                     }
-                                   ],
-                                 ReturnsVoid: true,
-                                 IsStatic: true,
-                                 ContainingType:
-                                 {
-                                     Name: var typeName,
-                                     ContainingNamespace: var @namespace,
-                                     TypeKind: var typeKind and (TypeKind.Class
-                                     or TypeKind.Struct
-                                     or TypeKind.Interface)
-                                 },
-                                 IsDefinition: true, //是否声明
-
-
-                             }
-                         })
-                     {
-                         return null;
-                     }
-
-                     return new GatheredData(methodName, parameterName, typeName, @namespace, typeKind, node);
-                 }
-
-                ).Collect(),
-                (spc, data) =>
+        context.RegisterSourceOutput(context.SyntaxProvider.ForAttributeWithMetadataName("SourceGeneratorSample.SayHello2Attribute"
+                ,
+                static (node, _) => node is MethodDeclarationSyntax
                 {
-
-
-                    foreach (var item in data)
+                    Modifiers: var modifiers and not [],
+                    Parent: TypeDeclarationSyntax
                     {
-                        if (item is not var (methodName, parameterName, typeName, @namespace, typeKind, node))
+                        Modifiers: var typeModifiers and not []
+                    }
+                } &&
+                                    modifiers.Any(SyntaxKind.PartialKeyword) &&
+                                    typeModifiers.Any(SyntaxKind.PartialKeyword),
+                (gasc, _) =>
+                {
+                    if (gasc is not
                         {
+                            TargetNode: MethodDeclarationSyntax node,
+                            TargetSymbol: IMethodSymbol
+                            {
+                                Name: var methodName,
+                                TypeParameters: [],
+                                Parameters:
+                                [
+                                    {
+                                        Type.SpecialType: SpecialType.System_String,
+                                        Name: var parameterName
+                                    }
+                                ],
+                                ReturnsVoid: true,
+                                IsStatic: true,
+                                ContainingType:
+                                {
+                                    Name: var typeName,
+                                    ContainingNamespace: var @namespace,
+                                    TypeKind: var typeKind and (TypeKind.Class
+                                    or TypeKind.Struct
+                                    or TypeKind.Interface)
+                                },
+                                IsDefinition: true //是否声明
+                            }
+                        })
+                    {
+                        return null;
+                    }
+                    return new GatheredData(methodName, parameterName, typeName, @namespace, typeKind, node);
+                }).Collect(),
+            (spc, data) =>
+            {
+                foreach (var item in data)
+                {
+                    if (item is not var (methodName, parameterName, typeName, @namespace, typeKind, node))
+                    {
+                        continue;
+                    }
+                    var tyepKindString = typeKind switch
+                    {
+                        TypeKind.Class => "class",
+                        TypeKind.Struct => "struct",
+                        TypeKind.Interface => "interface"
+                    };
 
-                            continue;
-                        }
-
-                        var tyepKindString = typeKind switch
-                        {
-                            TypeKind.Class => "class",
-                            TypeKind.Struct => "struct",
-                            TypeKind.Interface => "interface"
-                        };
-
-
-                        //ClassDeclarationSyntax
-
-                        var namespaceString = @namespace.ToDisplayString(SymbolDisplayFormat.FullyQualifiedFormat);
-                        namespaceString = namespaceString["global::".Length..]?.TrimEnd('>'); //global::
-
-                        var output = $@"
+                    //ClassDeclarationSyntax
+                    var namespaceString = @namespace.ToDisplayString(SymbolDisplayFormat.FullyQualifiedFormat);
+                    namespaceString = namespaceString["global::".Length..]?.TrimEnd('>'); //global::
+                    var output = $@"
 // <auto-generated/>  
 #nullable enable
 
@@ -108,13 +96,10 @@ static partial {tyepKindString}  {typeName}
     }}
 }}
 ";
-
-                        string extensionTextFormatted = CSharpSyntaxTree.ParseText(output).GetRoot().NormalizeWhitespace().SyntaxTree.GetText().ToString();
-                        spc.AddSource($"{typeName}{SourceGeneratorFileNameShortcut.GreetingGenerator_SayHello2Attribute_UseIncrementalGenerator}", SourceText.From(extensionTextFormatted, Encoding.UTF8));
-
-                    }
+                    var extensionTextFormatted = CSharpSyntaxTree.ParseText(output).GetRoot().NormalizeWhitespace().SyntaxTree.GetText().ToString();
+                    spc.AddSource($"{typeName}{SourceGeneratorFileNameShortcut.GreetingGenerator_SayHello2Attribute_UseIncrementalGenerator}", SourceText.From(extensionTextFormatted, Encoding.UTF8));
                 }
-                );
+            });
     }
 }
 
